6e44cd14e425c11fe4b67397a5abac02174e28c0
mottapedro <mottapedro01@gmail.com>
2023-03-16 17:30:17 -0300

    Frist commit

---
+++ valida-form.js
@@ -0,0 +1,72 @@
+//declar as constantes
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordtwo = document.getElementById('passwordtwo');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();//serve para previnor o comportamento
+
+    checkInputs();//vai executar a função de checagem dos inputs
+});
+
+function checkInputs() {
+    const usernameValue = username.value.trim()//remove os espaços em branco
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const passwordtwoValue = passwordtwo.value.trim()
+
+    if (usernameValue === '') {//é vazio ? é verdadeiro -> entra no if
+        //mostro erro
+        //adciono a class erro
+
+        erroValidation(username, 'Preencha o campo')
+    } else if (usernameValue.length <= 5) {
+        erroValidation(username, 'Esse campo deve conter no minimo 5 carcteres')
+
+    } else {
+        //cumpriu o requisito entrou no else 
+        succesValidation(username)
+    }
+
+    if (emailValue == '') {
+        erroValidation(email, 'Prencha o campo')
+    } else {
+        succesValidation(email)
+    }
+
+    if (password === '') {
+        erroValidation(password, 'Preencha o campo');
+    } else if (passwordValue.length <= 8) {
+        erroValidation(username, 'Esse campo deve conter no minimo 9 carcteres')
+    } else {
+        succesValidation(password);
+    }
+
+    if (passwordtwoValue === '') {
+        erroValidation(password, 'Preencha o campo');
+    } else if (passwordValue !== passwordtwoValue) {
+        erroValidation(password, 'A senha dever ser igual');
+    } else {
+        succesValidation(passwordtwo);
+    }
+}
+
+function erroValidation(input, message) {
+    const formControl = input.parentElement;//pega o elemento pai do input
+    const small = formControl.querySelector('small')//identifica a tag (small) do html
+
+    small.innerText = message//variavel
+    formControl.className = 'from-control error';
+}
+
+function succesValidation(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'from-control success'
+}
+
+
+
+
